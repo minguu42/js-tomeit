507a866f5cf667bce6b86c8df2444848ec5ff481
minguu <minguu42@gmail.com>
2021-06-16 00:51:30 +0900

    Noto Sans JP の導入とファビコンの設定

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
-import styles from 'styles/Home.module.css'
 
 const Home = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>tomeit</title>
-        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
       </Head>
       <main>
         Hello, World!
