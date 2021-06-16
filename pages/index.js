9f1f5a0c9c75ca1f350efa45621a89a76d879bb6
minguu <minguu42@gmail.com>
2021-06-17 07:19:04 +0900

    ボタンの追加

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import Header from 'components/Header'
 import styles from 'styles/Home.module.css'
+import Button from '../components/Button'
 
 const Home = () => {
   return (
@@ -11,8 +12,13 @@ const Home = () => {
       </Head>
       <Header />
       <main className={styles.welcomeLayout}>
-        <h2 className={styles.welcomeHeadline}>やるべきことをやる</h2>
-        <p className={styles.welcomeDescription}>tomeit は「今やるべきことだけを考え、行う」<br />をコンセプトにしたタスク管理アプリです。</p>
+        <div>
+          <h2 className={styles.welcomeHeadline}>やるべきことをやる</h2>
+          <p className={styles.welcomeDescription}>tomeit は「今やるべきことだけを考え、行う」<br />をコンセプトにしたタスク管理アプリです。</p>
+        </div>
+        <div>
+          <Button text='sign up' />
+        </div>
       </main>
     </div>
   )
