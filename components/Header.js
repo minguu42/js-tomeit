d45014db6fb4c445ddd6c1548cb3477b397fa314
minguu <minguu42@gmail.com>
2021-06-17 21:08:17 +0900

    Header の完成

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 import { useAuth } from 'lib/AuthContext'
@@ -6,26 +6,36 @@ import styles from 'styles/components/Header.module.css'
 import AccountCircle from 'components/icons/AccountCircle'
 import Summarize from './icons/Summarize'
 
-const Header = ({ currentUser, handleLogout }) => (
+const Header = ({ currentUser, isMenuDisplay, handleAccountCircleClick, handleLogout }) => (
   <header className={styles.container}>
     <div className={styles.inner}>
       <h6 className={styles.logo}>tomeit</h6>
       {currentUser &&
         <div className={styles.icons}>
           <span role='button' className={styles.icon}>
-            <AccountCircle fill='#fff' />
-          </span>
-          <span role='button' className={styles.icon}>
             <Summarize fill='#fff' />
           </span>
+          <div className={styles.accountMenu}>
+            <span onClick={handleAccountCircleClick} role='button' className={styles.icon}>
+            <AccountCircle fill='#fff' />
+          </span>
+            {isMenuDisplay &&
+                <button onClick={handleLogout} className={styles.menu}>Log out</button>
+            }
+          </div>
         </div>}
     </div>
   </header>
 )
 
 const HeaderContainer = ({ setLogoutError }) => {
+  const [isMenuDisplay, setIsMenuDisplay] = useState(false)
   const router = useRouter()
   const { currentUser, logout } = useAuth()
+
+  const handleAccountCircleClick = () => {
+    setIsMenuDisplay(isMenuDisplay => !isMenuDisplay)
+  }
 
   const handleLogout = async () => {
     setLogoutError('')
@@ -38,7 +48,11 @@ const HeaderContainer = ({ setLogoutError }) => {
   }
 
   return (
-    <Header currentUser={currentUser} handleLogout={handleLogout} />
+    <Header
+        isMenuDisplay={isMenuDisplay}
+        handleAccountCircleClick={handleAccountCircleClick}
+        currentUser={currentUser}
+        handleLogout={handleLogout} />
   )
 }
 
