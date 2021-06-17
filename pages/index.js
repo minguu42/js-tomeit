d31182b09032db1465f9a527148d2a46b693f3a4
minguu <minguu42@gmail.com>
2021-06-17 19:24:35 +0900

    Google ログインが可能になる

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import Head from 'next/head'
 
-import Header from 'components/Header'
+import { useAuth } from 'lib/AuthContext'
 import styles from 'styles/Home.module.css'
-import Button from '../components/Buttons'
+import Header from 'components/Header'
+import Button from 'components/Buttons'
 
 const Home = () => {
+  const { login } = useAuth()
+  const [error, setError] = useState('')
+
+  const handleClick = async () => {
+    try {
+      setError('')
+      await login()
+    } catch {
+      setError('Failed to login')
+    }
+  }
+
   return (
     <div>
       <Head>
@@ -12,14 +26,12 @@ const Home = () => {
       </Head>
       <Header />
       <main className={styles.layout}>
+        {error && <p className={styles.errorMessage}>ログインに失敗しました. もう一度お試しください.</p>}
         <div>
           <h2 className={styles.headline}>やるべきことをやる</h2>
           <p className={styles.description}>tomeit は「今やるべきことだけを考え、行う」<br />をコンセプトにしたタスク管理アプリです。</p>
         </div>
-        <div className={styles.buttons}>
-          <Button text='sign up' />
-          <Button text='log in' type='outline' />
-        </div>
+        <Button text='login' handleClick={handleClick} />
       </main>
     </div>
   )
