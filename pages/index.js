9580e0f08892ff6ad7c8b33f7730708185fb9593
minguu <minguu42@gmail.com>
2021-06-18 13:29:12 +0900

    Home と Landing でコンポーネントを分離

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 
 import { useAuth } from 'lib/AuthContext'
 import { useError } from 'lib/ErrorContext'
-import styles from 'styles/Home.module.css'
+import styles from 'styles/Index.module.css'
 import Header from 'components/Header'
 import Button from 'components/Buttons'
 
-const Home = () => {
+const Landing = () => {
   const { login } = useAuth()
-  const { error, setError } = useError()
+  const { setError } = useError()
 
   const handleClick = async () => {
     setError('')
@@ -20,21 +20,33 @@ const Home = () => {
   }
 
   return (
+    <main className={styles.layout}>
+      <div>
+        <h2 className={styles.headline}>やるべきことをやる</h2>
+        <p className={styles.description}>tomeit は「今やるべきことだけを考え、行う」<br />をコンセプトにしたタスク管理アプリです。</p>
+      </div>
+      <Button text='login' handleClick={handleClick} />
+    </main>
+  )
+}
+
+const Home = () => {
+}
+
+const Index = () => {
+  const { error } = useError()
+  const {currentUser} = useAuth()
+
+  return (
     <div>
       <Head>
         <title>tomeit</title>
       </Head>
       <Header />
-      <main className={styles.layout}>
-        {error && <p className={styles.errorMessage}>{error}</p>}
-        <div>
-          <h2 className={styles.headline}>やるべきことをやる</h2>
-          <p className={styles.description}>tomeit は「今やるべきことだけを考え、行う」<br />をコンセプトにしたタスク管理アプリです。</p>
-        </div>
-        <Button text='login' handleClick={handleClick} />
-      </main>
+      {error && <p className={styles.errorMessage}>{error}</p>}
+      {!currentUser && <Landing />}
     </div>
   )
 }
 
-export default Home
+export default Index
