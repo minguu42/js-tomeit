efcde69ce040ff6ad5851899e97677c06873e051
minguu <minguu42@gmail.com>
2021-06-18 10:40:27 +0900

    AccountMenu コンポーネントの作成

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-
 import { useAuth } from 'lib/AuthContext'
 import styles from 'styles/components/Header.module.css'
-import AccountCircle from 'components/icons/AccountCircle'
-import Summarize from './icons/Summarize'
+import AccountMenu from 'components/AccountMenu'
+import Summarize from 'components/icons/Summarize'
 
-const Header = ({ currentUser, isMenuDisplay, handleAccountCircleClick, handleLogout }) => (
+const Header = ({ currentUser }) => (
   <header className={styles.container}>
     <div className={styles.inner}>
       <h6 className={styles.logo}>tomeit</h6>
@@ -15,44 +12,19 @@ const Header = ({ currentUser, isMenuDisplay, handleAccountCircleClick, handleLo
           <span role='button' className={styles.icon}>
             <Summarize fill='#fff' />
           </span>
-          <div className={styles.accountMenu}>
-            <span onClick={handleAccountCircleClick} role='button' className={styles.icon}>
-            <AccountCircle fill='#fff' />
+          <span className={styles.icon}>
+            <AccountMenu />
           </span>
-            {isMenuDisplay &&
-                <button onClick={handleLogout} className={styles.menu}>Log out</button>
-            }
-          </div>
         </div>}
     </div>
   </header>
 )
 
-const HeaderContainer = ({ setLogoutError }) => {
-  const [isMenuDisplay, setIsMenuDisplay] = useState(false)
-  const router = useRouter()
-  const { currentUser, logout } = useAuth()
-
-  const handleAccountCircleClick = () => {
-    setIsMenuDisplay(isMenuDisplay => !isMenuDisplay)
-  }
-
-  const handleLogout = async () => {
-    setLogoutError('')
-    try {
-      await logout()
-      await router.push('/')
-    } catch {
-      setLogoutError('ログアウトに失敗しました。もう一度お試しください。')
-    }
-  }
+const HeaderContainer = () => {
+  const { currentUser } = useAuth()
 
   return (
-    <Header
-        isMenuDisplay={isMenuDisplay}
-        handleAccountCircleClick={handleAccountCircleClick}
-        currentUser={currentUser}
-        handleLogout={handleLogout} />
+    <Header currentUser={currentUser} />
   )
 }
 
