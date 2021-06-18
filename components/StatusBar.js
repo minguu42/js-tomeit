60c23a312049940deca38b878fc54eed08f506d4
minguu <minguu42@gmail.com>
2021-06-18 16:06:33 +0900

    StatusBar コンポーネントの作成

---
+++ components/StatusBar.js
@@ -0,0 +1,25 @@
+import styles from 'styles/components/StatusBar.module.css'
+
+const StatusBar = ({
+  countToNextRest,
+  remainingTaskNum,
+  todayTaskNum
+}
+) => (
+  <div className={styles.container}>
+    <div className={styles.item}>
+      <p className={styles.itemValue}>{countToNextRest}</p>
+      <p className={styles.itemKey}>休憩まで</p>
+    </div>
+    <div className={styles.item}>
+      <p className={styles.itemValue}>{remainingTaskNum}</p>
+      <p className={styles.itemKey}>残りタスク数</p>
+    </div>
+    <div className={styles.item}>
+      <p className={styles.itemValue}>{todayTaskNum}</p>
+      <p className={styles.itemKey}>今日のポモドーロ</p>
+    </div>
+  </div>
+)
+
+export default StatusBar
