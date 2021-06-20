ccdc0fb5d4a3a5c01efe5d0f146814db35bed050
minguu <minguu42@gmail.com>
2021-06-20 12:32:56 +0900

    新しいアイコンの追加

--- components/icons/Flag.js
+++ components/icons/Flag.js
@@ -6,8 +6,7 @@ const Flag = ({ size, fill }) => (
     width={size || 24}
     fill={fill || '#000'}
   >
-    <path d='M0 0h24v24H0V0z' fill='none' />
-    <path d='M12.36 6l.4 2H18v6h-3.36l-.4-2H7V6h5.36M14 4H5v17h2v-7h5.6l.4 2h7V6h-5.6L14 4z' />
+    <path d='M0 0h24v24H0V0z' fill='none' /><path d='M12.36 6l.4 2H18v6h-3.36l-.4-2H7V6h5.36M14 4H5v17h2v-7h5.6l.4 2h7V6h-5.6L14 4z' />
   </svg>
 )
 
