941d8616b7db4d01a06c74052875778ed947df2e
minguu <minguu42@gmail.com>
2021-06-20 12:34:04 +0900

    Task, TaskList コンポーネントの雛形の追加

---
+++ components/Task.js
@@ -0,0 +1,7 @@
+// import styles from 'styles/components/Task.module.css'
+
+const Task = () => (
+  <div />
+)
+
+export default Task
---
+++ components/TaskList.js
@@ -0,0 +1,5 @@
+const TaskList = () => (
+  <div />
+)
+
+export default TaskList
