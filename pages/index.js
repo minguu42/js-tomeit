3fc0469bd0a972056615e6677c0886f69edb5a51
minguu <minguu42@gmail.com>
2021-06-20 14:05:00 +0900

    Task コンポーネントの作成

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Header from 'components/Header'
 import Button from 'components/Buttons'
 import StatusBar from '../components/StatusBar'
 import AddTaskForm from '../components/AddTaskForm'
+import Task from '../components/Task'
 
 const NotLoggedIn = () => {
   const { login } = useAuth()
@@ -41,6 +42,30 @@ const LoggedIn = () => {
         todayTaskNum={0}
       />
       <AddTaskForm />
+      <Task
+        taskName='タスク1'
+        primary={0}
+        deadline='12月31日'
+        pomodoroCount={0}
+      />
+      <Task
+        taskName='タスク2'
+        primary={1}
+        deadline='12月3日'
+        pomodoroCount={1}
+      />
+      <Task
+        taskName='タスク3'
+        primary={2}
+        deadline='6月2日'
+        pomodoroCount={2}
+      />
+      <Task
+        taskName='タスク4'
+        primary={3}
+        deadline=''
+        pomodoroCount={10}
+      />
     </div>
   )
 }
